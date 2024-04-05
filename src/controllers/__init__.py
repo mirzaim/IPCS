693ddbb5305e182c2b18eb3b94f9b9e216974aab
Morteza Mirzaei <mortezamirzai120@gmail.com>
2024-04-05 11:01:09 -0400

    Add error handling for controller selector.

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -3,12 +3,16 @@ from controllers.base import Controller
 from controllers.fuzzy import FuzzyController
 from controllers.pid import PIDController
 
+
 list_of_controllers = {
     "base": Controller,
     "fuzzy": FuzzyController,
     "pid": PIDController,
 }
 
-def get_controller(name, **config):
-    return list_of_controllers[name](**config)
 
+def get_controller(name, **config):
+    if name not in list_of_controllers:
+        raise ValueError(f"Controller {name} not found")
+
+    return list_of_controllers[name](**config)
