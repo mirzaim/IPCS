0da21e11956872a4c76c423dd575496c0b20c1ad
Morteza Mirzaei <mortezamirzai120@gmail.com>
2024-04-15 18:54:48 -0400

    Update fuzzy.py.

--- src/controllers/fuzzy.py
+++ src/controllers/fuzzy.py
@@ -150,29 +150,3 @@ class FuzzyController(Controller):
         result = s_mu_x / s_mu
 
         return result
-
-# class FuzzyController:
-
-#     def __init__(self, fcl_path):
-#         self.system = Reader().load_from_file(fcl_path)
-
-
-#     def _make_input(self, world):
-#         return dict(
-#             cp = world.x,
-#             cv = world.v,
-#             pa = degrees(world.theta),
-#             pv = degrees(world.omega)
-#         )
-
-
-#     def _make_output(self):
-#         return dict(
-#             force = 0.
-#         )
-
-
-#     def decide(self, world):
-#         output = self._make_output()
-#         self.system.calculate(self._make_input(world), output)
-#         return output['force']
